ea9cdf4f226ffd1ac4434d9f8a3182b77ba9506e
Eduard Ibrahimi <eduard.ibrahimi@edu.tbz.ch>
2023-11-10 14:13:51 +0100

    kn03_und_kn04

---
+++ Kn04/Aufgabe_B/image.php
@@ -0,0 +1,13 @@
+	<?php $ihrname = "Eduard Ibrahimi"; ?>
+<html>
+	<head>
+	<title><?php echo($ihrname) ?></title>
+	</head>
+	<body>
+		Schöner Meeresblick mit einem Pool.
+	<br />
+	<img src="s3://eduards3bucket/illia-plakhuta-V1gfvXw1g7I-unsplash.jpg" />
+	
+	Bild von Unsplash.
+	</body>
+</html>
